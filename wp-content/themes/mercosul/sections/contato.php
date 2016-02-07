f7da92e291b5b2a76f5a40a4ad4f445700d4a818
Filipe Molina <filipemolina@live.com>
2016-02-07 20:34:59 -0200

    Formulário de Contato e Mapa

--- wp-content/themes/mercosul/sections/contato.php
+++ wp-content/themes/mercosul/sections/contato.php
@@ -4,13 +4,13 @@
 		
 		<div class="row">
 
-			<!-- Formulário de Contato -->
+			<!-- Informações de Contato -->
 			
 			<div class="col-md-6 preto">
 				
-				<div class="info">
+				<div class="info col-md-offset-4">
 					
-					<h3 class="grotesk_demi">DISTRIBUIDORA MERCOSUL</h3>
+					<h3 class="grotesk_ultra">DISTRIBUIDORA MERCOSUL</h3>
 
 					<div class="endereco">
 						<span class="glyphicon glyphicon-home" aria-hidden="true"></span>
@@ -34,11 +34,45 @@
 
 				</div>
 
+				<!-- Formulário de Contato -->
+
+				<div class="formulario col-md-offset-4">
+					
+					<h3 class="grotesk_ultra">FALE CONOSCO</h3>
+
+					<form action="" class="form_contato">
+						
+						<div class="row">
+							<input type="text" placeholder="Nome" class="nome col-md-8 clear" id="nome">
+						</div>
+
+						<div class="row">
+							<input type="email" placeholder="E-mail" class="email col-md-8" id="email">
+						</div>
+
+						<div class="row">
+							<input type="text" placeholder="Assunto" class="assunto col-md-8" id="assunto">
+						</div>
+
+						<div class="row">
+							<textarea name="mensagem" class="mensagem col-md-8" id="mensagem" cols="30" rows="5" placeholder="Mensagem"></textarea>
+						</div>
+
+					</form>
+
+				</div>
+
 			</div>
 
 			<!-- Mapa -->
 
 			<div class="col-md-6 mapa">
+
+				<div class="row">
+					
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3679.2357008732392!2d-43.44446548505328!3d-22.756632538251335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x996654e981bf63%3A0xefd265da06e02891!2sR.+Cel.+Francisco+Soares%2C+739+-+Centro%2C+Nova+Igua%C3%A7u+-+RJ%2C+26220-031!5e0!3m2!1sen!2sbr!4v1454883968784" width="100%" height="415" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+				</div>
 				
 			</div>
 
